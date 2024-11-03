b401708d1a0d55769c52043a0ef2941e1a359b1f
Moroni Bamvakiades Ramos <84743417+moronibr@users.noreply.github.com>
2024-11-02 23:54:14 -0300

    Update getdates.js

---
+++ scripts/getdates.js
@@ -0,0 +1,26 @@
+// select the DOM elements for output
+const full = document.querySelector("#full");
+const worldfull = document.querySelector("#world-full");
+const short = document.querySelector("#short");
+const medium = document.querySelector("#medium");
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const dayofweek = document.querySelector("#dayofweek");
+
+// use the date object
+const today = new Date();
+
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+worldfull.innerHTML = `BRAZIL: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-UK",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+
