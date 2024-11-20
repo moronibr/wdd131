3a4da49a3d807e06fd4b0ff6121ab10826a00f09
moroni <moroni.ramos@gmail.com>
2024-11-19 23:23:08 -0300

    Legal

---
+++ temples.js
@@ -0,0 +1,5 @@
+// Toggle the menu visibility on hamburger click
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('visible');  // Toggle the 'visible' class to show/hide the menu
+}
